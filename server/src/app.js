a8f8ec410d62b22c819370b8983954d689942417
Yahya305 <saimyahya47@gmail.com>
2025-03-02 02:52:12 +0500

    fix: minor bug

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import ErrorMiddleware from "./router/ErrorRouter";
+import ErrorMiddleware from "./router/ErrorRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
