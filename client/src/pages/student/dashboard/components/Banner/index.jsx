be267a062753f78e9a4b89034b79e4b9e66b52d7
AunHassan <hassanaun520@gmail.com>
2025-03-05 17:16:16 +0500

    banner card and table component completed

---
+++ client/src/pages/student/dashboard/components/Banner/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Banner/index.css'
+
+const index = () => {
+  return (
+    <div className='banner-section'>
+        <div className="banner-wrapper">
+           <h2 className='heading'>Welcome To IAD Dashboard</h2>
+        <p className="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </div>
+  )
+}
+
+export default index
--- client/src/pages/student/dashboard/index.jsx
+++ client/src/pages/student/dashboard/index.jsx
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 import "./index.css";
 import Table from "./components/Table";
 import Card from "./components/Card";
+import Banner from "./components/Banner"
 
 function StudentDashboard() {
     const [count, setCount] = useState(5);
 
     return (
         <div>
-            {/* <Banner /> */}
+            <Banner />
             <Table />
             <Card />
+            {/* <Card />
             <Card />
-            <Card />
-            <Card />
+            <Card /> */}
             
         </div>
     );
