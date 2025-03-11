0b11f15dfa6ef767a50f5fd326dbe1cdfcf540ec
Moin UD-din <129534945+MoinUddin-dev-dot@users.noreply.github.com>
2025-03-11 11:05:06 +0500

    Responsive Navbar

--- client/src/pages/LandingPage/_lib/Header/NavBar.jsx
+++ client/src/pages/LandingPage/_lib/Header/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import NavBars from "./NavBars";
 import NavButton from "./NavButton";
@@ -6,19 +6,29 @@ import "./index.css";
 import Images from "../Images";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav>
+    <>
+      <nav>
         <Logo />
         <NavBars />
-        <NavButton 
-          name="Login Now" 
-          image="/images/circularArrow.svg" 
+        <NavButton
+          name="Login Now"
+          image="/images/circularArrow.svg"
           to="/student/login"
         />
-        <button  class="hamburger">
-        ☰
-      </button>
-    </nav>
+
+        <button class="hamburger" onClick={() => setMenuOpen(true)}>
+          ☰
+        </button>
+      </nav>
+      <div className={`overlay ${menuOpen ? "show" : ""}`}>
+        <button className="close-btn" onClick={() => setMenuOpen(false)}>
+          ✖
+        </button>
+        <NavBars />
+      </div>
+    </>
   );
 }
 
