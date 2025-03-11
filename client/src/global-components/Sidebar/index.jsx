dc79955a0d8fa5951871955fca3c5b3b1677b499
Hamza Siddiqui <hamzapro285@gmail.com>
2025-03-12 01:06:37 +0500

    Basic UI changes

--- client/src/global-components/Sidebar/index.jsx
+++ client/src/global-components/Sidebar/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useNavigate, useLocation } from "react-router"
+import { CiSettings } from "react-icons/ci";
+import { SlLogout } from "react-icons/sl";
+
 import "./index.css"
 
 
@@ -26,8 +29,20 @@ const Sidebar = ({ routes }) => {
             <div className="user">
                 <hr />
                 <div className="role">Admin</div>
-                <div className="profile"></div>
-                <div className="actions"></div>
+                <div className="profile">
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" alt="" />
+                    <div className="name">Yahya Hamza</div>
+                </div>
+                <div className="actions">
+                    <button>
+                        <CiSettings fontSize={30} />
+                        <span>Settings</span>
+                    </button>
+                    <button>
+                        <SlLogout fontSize={20} />
+                        <span>Log out</span>
+                    </button>
+                </div>
             </div>
         </aside>
     )
