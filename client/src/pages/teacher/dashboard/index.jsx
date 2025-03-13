256637ec97f96d6bf29aa76e6887adb6e00b6ec2
Hamza Siddiqui <hamzapro285@gmail.com>
2025-03-14 00:19:36 +0500

    feat: Teacher Dashboard Structure done

--- client/src/pages/teacher/dashboard/index.jsx
+++ client/src/pages/teacher/dashboard/index.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import Sidebar from '../../../global-components/Sidebar'
+import { FaHome } from 'react-icons/fa'
+import DashboardSearchBar from '../../../global-components/DashboardSearchBar'
+import { Outlet } from 'react-router'
 
-function TeacherDashboard() {
+const Routes = [
+  {
+    path: "/teacher/dashboard",
+    name: "Dashboard",
+    icon: <FaHome />
+  },
+  {
+    path: "/teacher/student-grading",
+    name: "Student Grading",
+    icon: <FaHome />
+  },
+]
+function TeacherDashboardStructure() {
   return (
-    <div>
-      Teacher Dsaash
+    <div className="teacher-dashboard dashboard-structure">
+      <Sidebar routes={Routes} />
+      <div className="dashboard" style={{ position: "relative" }}>
+        <DashboardSearchBar />
+        <Outlet />
+      </div>
     </div>
   )
 }
 
-export default TeacherDashboard
+export default TeacherDashboardStructure
--- client/src/routes/route.jsx
+++ client/src/routes/route.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router";
 
 import LandingPage from "../pages/LandingPage/Main.jsx";
-import TeacherDashboard from "../pages/teacher/dashboard";
+import TeacherDashboardStructure from "../pages/teacher/dashboard";
 import Loginpage from "../pages/student/authentication/Login";
 import Signup from "../pages/student/authentication/signup";
 import ProjectGradingPage from "../pages/teacher/projectGrading/projectGrading";
 
 //  Student Routes
-import StudentDashboardStructure  from "../pages/student/dashboard";
+import StudentDashboardStructure from "../pages/student/dashboard";
 import StudentMainDashboardPage from "../pages/student/dashboard/dashboard-inner-pages/MainDashboard";
 import PerformanceAnalysisPage from "../pages/student/dashboard/dashboard-inner-pages/PerformanceAnalysis";
 import StudentProgressPage from "../pages/student/dashboard/dashboard-inner-pages/StudentProgress";
@@ -21,16 +21,16 @@ function Router() {
 
             {/* Nested student routes */}
             <Route path="/student" element={<StudentDashboardStructure />}>
-                <Route path="dashboard"  element={<StudentMainDashboardPage />}/>
-                <Route path="performance-analysis"  element={<PerformanceAnalysisPage />}/>
-                <Route path="std-progress"  element={<StudentProgressPage />}/>
+                <Route path="dashboard" element={<StudentMainDashboardPage />} />
+                <Route path="performance-analysis" element={<PerformanceAnalysisPage />} />
+                <Route path="std-progress" element={<StudentProgressPage />} />
                 <Route path="login" element={<Loginpage />} />
                 <Route path="signup" element={<Signup />} />
             </Route>
 
             {/* Nested teacher routes */}
-            <Route path="/teacher">
-                <Route path="dashboard" element={<TeacherDashboard />} />
+            <Route path="/teacher" element={<TeacherDashboardStructure />} >
+                <Route path="dashboard" element={<StudentMainDashboardPage />} />
                 <Route
                     path="student-grading"
                     element={<ProjectGradingPage />}
