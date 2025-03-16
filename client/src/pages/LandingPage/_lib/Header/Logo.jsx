b29a97f7932b0a3c0911d30b28832a173d9a0ccb
Yahya305 <saimyahya47@gmail.com>
2025-03-17 03:51:28 +0500

    fix: logo path

--- client/src/pages/LandingPage/_lib/Header/Logo.jsx
+++ client/src/pages/LandingPage/_lib/Header/Logo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import kokImage from "../../../../assets/kok.jpeg";
 
 function Logo() {
     return (
         <div className="logo">
             <img
                 width={53}
-                src="/images/kok.jpeg"
+                src={kokImage}
                 alt="IAD logo"
                 style={{ borderRadius: "50%" }}
             />
