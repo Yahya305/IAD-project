f50032ecf5a3b7b1447cf996e4538556230eabb0
Yahya305 <saimyahya47@gmail.com>
2025-03-17 01:44:57 +0500

    Update Logo.jsx

--- client/src/pages/LandingPage/_lib/Header/Logo.jsx
+++ client/src/pages/LandingPage/_lib/Header/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
    
         <div className="logo">
-          <img width={53} src="/images/kok.jpeg" alt="Netflix logo" style={{borderRadius:"50%"}} />
+          <img width={53} src="/images/kok.jpeg" alt="IAD logo" style={{borderRadius:"50%"}} />
         </div>
         
       
