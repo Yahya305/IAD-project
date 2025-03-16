684bd46d13a1a3615979a70de18c7a27b965b49f
Yahya305 <saimyahya47@gmail.com>
2025-03-16 08:28:28 +0500

    add: students data in csv seeding

--- server/src/config/setupApplication.js
+++ server/src/config/setupApplication.js
@@ -1,7 +1,11 @@
 import TeacherService from "../service/TeacherService.js";
 import { seedStudentsData } from "../utils/seedStudentsData.js";
+import prisma from "./prismaConfig.js";
 
 export const setupApplication = async () => {
+    // await prisma.student.deleteMany({});
+    // await prisma.teacher.deleteMany({});
+    // await prisma.team.deleteMany({});
 
     await TeacherService.setupTeacherConsole();
 
